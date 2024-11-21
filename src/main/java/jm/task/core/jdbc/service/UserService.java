16880cb22a4adffd393ff7f087b3a7661c7dadb1
Ishenko Alla <allaseva100@mail.ru>
2024-11-21 16:40:39 +0300

    test2

--- src/main/java/jm/task/core/jdbc/service/UserService.java
+++ src/main/java/jm/task/core/jdbc/service/UserService.java
@@ -1,4 +1,18 @@
 package jm.task.core.jdbc.service;
+import java.util.List;
+
+import jm.task.core.jdbc.model.User;
 
 public interface UserService {
+    void createUsersTable();
+
+    void dropUsersTable() ;
+
+    void saveUser(String name, String lastName, byte age);
+
+    void removeUserById(long id);
+
+    List<User> getAllUsers();
+
+    void cleanUsersTable();
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,4 +1,27 @@
 package jm.task.core.jdbc.util;
 
-public class Util {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public  class Util {
+    private static final String URL = "jdbc:mysql://localhost:3306/newtest?serverTimezone=Europe/Moscow&useSSL=false";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnection() throws ClassNotFoundException {
+
+        Connection connection = null;
+        try {
+            Class.forName(DRIVER).newInstance();
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+
 }
