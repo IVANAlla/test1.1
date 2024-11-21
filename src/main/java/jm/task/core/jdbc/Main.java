c27277c33f0498b12721e2e2d6fbaba7d6dc6b4d
Ishenko Alla <allaseva100@mail.ru>
2024-11-21 10:20:49 +0300

    перелелала

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,4 +1,25 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args)  {
+
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Name1", "LastName1", (byte) 33);
+        userService.saveUser("Name2", "LastName2", (byte) 25);
+        userService.saveUser("Name3", "LastName3", (byte) 31);
+        userService.removeUserById(1);
+        List<User> users = userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+    }
 }
+
+
+
